e3910830ae055cc2184c9c0e64c6488885ffbc33
Tipu Sultan <tipu.ru.cse@gmail.com>
2023-02-18 20:37:02 +0000

    added api and roomdb dao

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utility/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.utility
+
+class Constants {
+    companion object {
+        const val API_KEY = "3964fd8f2e49457e892799ba1e8badaa"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
